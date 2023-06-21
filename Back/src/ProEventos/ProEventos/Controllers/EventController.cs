5eec33262d1ddeb58586090e64d607bbdfae59bf
RicardoAlves1985 <ricardoalves1985@gmail.com>
2023-06-20 21:38:06 -0300

    [FEAT]- Create new structure

---
+++ Back/src/ProEventos/ProEventos/Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return new Event[]
+            {
+               new Event()
+               { EventId = 1,
+                Theme = "Angular 11",
+                Local = "BH",
+                Lote = "1",
+                PeopleCount = "250",
+                EventDate = DateTime.Now.AddDays(2).ToString()
+               }
+            };
+        }
+    }
+}
+
---
+++ Back/src/ProEventos/ProEventos/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Local { get; set; }
+        public string EventDate { get; set; }
+        public string Theme { get; set; }
+        public string PeopleCount { get; set; }
+        public string Lote { get; set; }
+        public string URLImage { get; set; }
+    }
+}
