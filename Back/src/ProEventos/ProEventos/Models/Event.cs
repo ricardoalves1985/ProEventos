c019f05773caff1d6c6cc0a5fec91dbebde5aaf8
RicardoAlves1985 <ricardoalves1985@gmail.com>
2023-06-22 21:08:04 -0300

    [Testing]- Testing new repository

--- Back/src/ProEventos/ProEventos/Models/Event.cs
+++ Back/src/ProEventos/ProEventos/Models/Event.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace ProEventos.Models
 {
     public class Event
-    {
+    { 
         public int EventId { get; set; }
         public string Local { get; set; }
         public string EventDate { get; set; }
@@ -14,5 +14,6 @@ namespace ProEventos.Models
         public string PeopleCount { get; set; }
         public string Lote { get; set; }
         public string URLImage { get; set; }
+        //testando new repository
     }
 }
