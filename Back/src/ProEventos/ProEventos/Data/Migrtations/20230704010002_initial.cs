891eba4bd127fadb88829232574f120a16edb66f
RicardoAlves1985 <ricardoalves1985@gmail.com>
2023-07-03 22:13:23 -0300

    #[FEAT]- Create Migrations

---
+++ Back/src/ProEventos/ProEventos/Data/Migrtations/20230704010002_initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProEventos.Data.Migrtations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: true),
+                    EventDate = table.Column<string>(type: "TEXT", nullable: true),
+                    Theme = table.Column<string>(type: "TEXT", nullable: true),
+                    PeopleCount = table.Column<string>(type: "TEXT", nullable: true),
+                    Lote = table.Column<string>(type: "TEXT", nullable: true),
+                    URLImage = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
