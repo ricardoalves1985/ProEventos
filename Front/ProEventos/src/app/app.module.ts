b51e16ecd1be0ec0dd0e77d789880617019c498f
RicardoAlves1985 <ricardoalves1985@gmail.com>
2023-07-18 23:32:25 -0300

    #[FEAT]-CreateDbcontext and some refacts and booststrap

--- Front/ProEventos/src/app/app.module.ts
+++ Front/ProEventos/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    SpeakersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
